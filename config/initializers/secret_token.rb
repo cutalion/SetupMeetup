4c6623b529d6d9435825d58c89e3e7b25988e88a
Alexander Glushkov <cutalion@gmail.com>
2012-04-10 01:15:13 +0300

    Added Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SetupMeetup::Application.config.secret_token = '60554ffda05aa46cde55de603208c008863da6b14a431f72203a23fb3e63d575094aeab0c0bb1d7114a8256a956b29f27d29ebb5aaa77eead57b3a4863d9ab74'
