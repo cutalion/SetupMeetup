0e62d3cf2d03e0b62a71ad055a3f0a049914dcf0
Ivan Linko <linko.ivan@hmail.com>
2012-04-20 19:44:09 +0300

    setup backbone

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem "omniauth-openid"
 gem "inherited_resources"
 gem "simple_form"
 
+gem 'backbone-on-rails'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require_tree .
+//= require underscore
+//= require backbone
+//= require ./setup_meetup
+//= require_tree ../templates/
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
