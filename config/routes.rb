e0158816965a7087aea4857670676e87b93d21b5
Yuri Omelchuk <jurgen@upscript.com>
2012-04-27 19:34:12 +0300

    changed from fluid to fixed grid layout welcome message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SetupMeetup::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root to: "landing#index"
+
   resources :events do
     member { put :join }
   end
