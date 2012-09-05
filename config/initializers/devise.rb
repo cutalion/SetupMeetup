c939119d7150ebe808f36414d5bd47673633348e
Alexander Glushkov <cutalion@gmail.com>
2012-09-05 15:57:25 +0300

    Don't show sign in with Twitter and FB buttons

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -208,9 +208,9 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   # config.omniauth :facebook, ENV['FACEBOOK_SETUPMEETUP_APP_ID'], ENV['FACEBOOK_SETUPMEETUP_SECRET']
   # config.omniauth :twitter, ENV['TWITTER_SETUPMEETUP_KEY'], ENV['TWITTER_SETUPMEETUP_SECRET']
-  config.omniauth :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), 
-                            :name => 'google', 
-                            :identifier => 'https://www.google.com/accounts/o8/id', 
+  config.omniauth :open_id, :store => OpenID::Store::Filesystem.new('/tmp'),
+                            :name => 'google',
+                            :identifier => 'https://www.google.com/accounts/o8/id',
                             :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}},
                             :require => 'omniauth-openid'
 
